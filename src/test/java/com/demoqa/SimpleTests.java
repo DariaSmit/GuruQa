0caff95bd680e09eefd6c03876aec75943746ea8
darya <DariaSmit@yandex.ru>
2022-08-12 17:41:11 +0300

    add Test

--- src/test/java/com/demoqa/SimpleTests.java
+++ src/test/java/com/demoqa/SimpleTests.java
@@ -2,7 +2,6 @@ package com.demoqa;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
